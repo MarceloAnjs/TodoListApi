6b10f82b1da87e64e8dab484c6a6f11c3baeb5f0
Marcelo Augusto <marcelo@sisacinova.com.br>
2023-05-17 09:21:58 -0300

    Setado os campos os campos que seram usado no dto de login

--- src/usuarios/dto/login-usuario.dto.ts
+++ src/usuarios/dto/login-usuario.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUsuarioDto } from './create-usuario.dto';
 
-export class LoginUsuarioDto extends PartialType(CreateUsuarioDto) {}
+export class LoginUsuarioDto extends PartialType(CreateUsuarioDto) {
+    Email: string;
+    Senha: string;
+}
