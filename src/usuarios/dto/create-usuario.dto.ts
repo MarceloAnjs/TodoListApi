9f4d193b1754b28aa6d8498c01b2b8a25bfc6d36
Marcelo Augusto <marcelo@sisacinova.com.br>
2023-05-21 15:20:30 -0300

    validacoes

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,7 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Usuario } from "../entities/usuario.entity";
+import { IsEmail, IsString } from "class-validator";
 
 export class CreateUsuarioDto implements Usuario {
+    @ApiProperty()
+    @IsString()
     Nome: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsEmail()
     Email: string;
+    
+    @ApiProperty()
+    @IsString()
     Senha: string;
 }
--- src/usuarios/dto/login-usuario.dto.ts
+++ src/usuarios/dto/login-usuario.dto.ts
@@ -1,7 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUsuarioDto } from './create-usuario.dto';
+import { IsEmail, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginUsuarioDto extends PartialType(CreateUsuarioDto) {
+    @ApiProperty()
+    @IsString()
+    @IsEmail()
     Email: string;
+
+    @ApiProperty()
+    @IsString()
     Senha: string;
 }
